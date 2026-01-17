7c79f94728778ff28b5b91cb817084345ecd1215
Bhuvan <baradhya95@gmail.com>
2026-01-17 22:35:00 +0530

    17th commit

--- modules/main.tf
+++ modules/main.tf
@@ -9,13 +9,9 @@ module "m2" {
   f1     = "m2f1"
   c1     = "m2c1"
 }
-output m1f1 {
-	value = module.m1.o1
+output "m1f1" {
+  value = module.m1.o1
 }
 variable "m1f1" {
   default = "m1f1"
-}
-module "vnet_example_all_default" {
-  source  = "Azure/vnet/azurerm//examples/all_default"
-  version = "5.0.1"
 }

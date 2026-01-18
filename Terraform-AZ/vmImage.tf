a5fa4c1aefbda92207bc1f73941523a3ffaad411
Bhuvan Aradhya L <96514404+bhuvan-aradhya-l@users.noreply.github.com>
2026-01-18 11:51:30 +0530

    Comment out VM and image resource definitions
    Comment out data and resource blocks for azurerm_virtual_machine and azurerm_image.

--- Terraform-AZ/vmImage.tf
+++ Terraform-AZ/vmImage.tf
@@ -1,11 +1,11 @@
-data "azurerm_virtual_machine" "vmtfBAL" {
-  name                = "vm-tf-BAL"
-  resource_group_name = "az-tf-BAL"
-}
+//data "azurerm_virtual_machine" "vmtfBAL" {
+//  name                = "vm-tf-BAL"
+//  resource_group_name = "az-tf-BAL"
+//}
 
-resource "azurerm_image" "vmImageBAL" {
-  name                      = "vm-tf-Image-BAL"
-  location                  = data.azurerm_virtual_machine.vmtfBAL.location
-  resource_group_name       = data.azurerm_virtual_machine.vmtfBAL.resource_group_name
-  source_virtual_machine_id = data.azurerm_virtual_machine.vmtfBAL.id
-}
+//resource "azurerm_image" "vmImageBAL" {
+//  name                      = "vm-tf-Image-BAL"
+//  location                  = data.azurerm_virtual_machine.vmtfBAL.location
+//  resource_group_name       = data.azurerm_virtual_machine.vmtfBAL.resource_group_name
+//  source_virtual_machine_id = data.azurerm_virtual_machine.vmtfBAL.id
+//}

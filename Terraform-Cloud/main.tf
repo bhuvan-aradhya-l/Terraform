99fc1dae0ca1b6564778a9658a65f1746b2bef5c
Bhuvan Aradhya L <96514404+bhuvan-aradhya-l@users.noreply.github.com>
2026-01-20 00:07:49 +0530

    Remove sensitive Azure credentials from main.tf
    Removed sensitive Azure provider credentials from main.tf.

--- Terraform-Cloud/main.tf
+++ Terraform-Cloud/main.tf
@@ -9,8 +9,4 @@ terraform {
 
 provider "azurerm" {
         features {}
-                        client_id=" c7f3b9ee-e1dc-477c-93e6-7e6d6cbd9529"
-                        client_secret=" to68Q~4mURQ60va8BsFkzOO.L5I1UwZIWjJjwdnl "
-                        tenant_id=" fb7f402e-a854-4429-92fe-3ed1e88e402a "
-                        subscription_id="53d10f2d-7d56-4d45-a103-34e5a1bce523"
 }

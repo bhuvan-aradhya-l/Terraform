49f09ae54b8ecdcccfbf5772a6b9b04fecb80e27
Bhuvan <baradhya95@gmail.com>
2026-01-19 23:50:05 +0530

    Terraform Cloud

---
+++ Terraform-Cloud/cloud.tf
@@ -0,0 +1,10 @@
+terraform { 
+  cloud { 
+    
+    organization = "bhuvan_B12" 
+
+    workspaces { 
+      name = "rg" 
+    } 
+  } 
+}
---
+++ Terraform-Cloud/var.tf
@@ -0,0 +1,10 @@
+//resource group variables
+variable "location" {
+  default = "Northeurope"
+  type    = string
+}
+
+variable "rg_name" {
+  default = "tfcloud-BAL"
+  type    = string
+}
